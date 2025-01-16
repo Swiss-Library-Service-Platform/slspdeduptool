efeb81dc1efee799bb23cc60a0367db7c34624fa
Raphaël Rey <raphael.rey@slsp.ch>
2025-01-17 00:25:02 +0100

    update structure

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slsptools.settings')
+
+    if os.getenv('env') == 'prod':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slsptools.settings_prod')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slsptools.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
