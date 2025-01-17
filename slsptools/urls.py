d431b56cb2b9763d5406573842fd7a8e67e87a5c
Raphaël Rey <raphael.rey@slsp.ch>
2025-01-17 22:38:34 +0100

    update structure

--- slsptools/urls.py
+++ slsptools/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('dedup/', include('dedup.urls')),
+    path('dedup/', include('dedup.urls')),
     path('admin/', admin.site.urls),
 ]
